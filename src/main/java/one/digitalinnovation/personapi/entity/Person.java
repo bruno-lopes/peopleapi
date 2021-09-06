dd06e58fa31d259da881db16d2cb2fee3b667f42
Bruno Lorenço Lopes <bruno.lorenco.lopes@gmail.com>
2021-09-06 20:24:02 -0300

    Mapeamento de classes de domínio

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,45 @@
+package one.digitalinnovation.personapi.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private LocalDateTime birthDate;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    List<Phone> phones;
+
+
+}
